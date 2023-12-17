f40c056bc7552216961be87b3c1106e8eadb9bab
Sahil-Chauhan99 <sahilchauhan.0619@gmail.com>
2023-12-17 19:34:16 +0530

    CarOps

---
+++ src/main/java/com/carops/CarOpsApplication.java
@@ -0,0 +1,13 @@
+package com.carops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carops/CarOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
